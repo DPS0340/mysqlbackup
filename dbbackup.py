64bf1a401567b406a67a8431cfac3459736cf532
DPS0340 <jh001007@gmail.com>
2019-01-06 15:45:27 +0900

    main

--- dbbackup.py
+++ dbbackup.py
@@ -26,62 +26,61 @@ import pickle
 
 # MySQL database details to which backup to be done. Make sure below user having enough privileges to take databases backup.
 # To take multiple databases backup, create any file like /backup/dbnames.txt and put databases names one on each line and assigned to DB_NAME variable.
-
-with open("./settings", "rb") as r:
+if __name__ == "__main__":
+    with open("./settings", "rb") as r:
         dump = pickle.load(r)
+    DB_HOST = dump["DB_HOST"]
+    DB_USER = dump["DB_USER"]
+    DB_USER_PASSWORD = dump["DB_USER_PASSWORD"]
+    DB_NAME = dump["DB_NAME"]
+    BACKUP_PATH = './backup'
 
-DB_HOST = dump["DB_HOST"]
-DB_USER = dump["DB_USER"]
-DB_USER_PASSWORD = dump["DB_USER_PASSWORD"]
-DB_NAME = dump["DB_NAME"]
-BACKUP_PATH = './backup'
+    # Getting current DateTime to create the separate backup folder like "20180817-123433".
+    DATETIME = time.strftime('%Y%m%d-%H%M%S')
+    TODAYBACKUPPATH = BACKUP_PATH + '/' + DATETIME
 
-# Getting current DateTime to create the separate backup folder like "20180817-123433".
-DATETIME = time.strftime('%Y%m%d-%H%M%S')
-TODAYBACKUPPATH = BACKUP_PATH + '/' + DATETIME
+    # Checking if backup folder already exists or not. If not exists will create it.
+    try:
+        os.stat(TODAYBACKUPPATH)
+    except:
+        os.mkdir(TODAYBACKUPPATH)
 
-# Checking if backup folder already exists or not. If not exists will create it.
-try:
-    os.stat(TODAYBACKUPPATH)
-except:
-    os.mkdir(TODAYBACKUPPATH)
+    # Code for checking if you want to take single database backup or assinged multiple backups in DB_NAME.
+    print ("checking for databases names file.")
+    if os.path.exists(DB_NAME):
+        file1 = open(DB_NAME)
+        multi = 1
+        print ("Databases file found...")
+        print ("Starting backup of all dbs listed in file " + DB_NAME)
+    else:
+        print ("Databases file not found...")
+        print ("Starting backup of database " + DB_NAME)
+        multi = 0
 
-# Code for checking if you want to take single database backup or assinged multiple backups in DB_NAME.
-print ("checking for databases names file.")
-if os.path.exists(DB_NAME):
-    file1 = open(DB_NAME)
-    multi = 1
-    print ("Databases file found...")
-    print ("Starting backup of all dbs listed in file " + DB_NAME)
-else:
-    print ("Databases file not found...")
-    print ("Starting backup of database " + DB_NAME)
-    multi = 0
+    # Starting actual database backup process.
+    if multi:
+    in_file = open(DB_NAME,"r")
+    flength = len(in_file.readlines())
+    in_file.close()
+    p = 1
+    dbfile = open(DB_NAME,"r")
 
-# Starting actual database backup process.
-if multi:
-   in_file = open(DB_NAME,"r")
-   flength = len(in_file.readlines())
-   in_file.close()
-   p = 1
-   dbfile = open(DB_NAME,"r")
+    while p <= flength:
+        db = dbfile.readline()   # reading database name from file
+        db = db[:-1]         # deletes extra line
+        dumpcmd = "mysqldump -h " + DB_HOST + " -u " + DB_USER + " -p" + DB_USER_PASSWORD + " " + db + " > " + pipes.quote(TODAYBACKUPPATH) + "/" + db + ".sql"
+        os.system(dumpcmd)
+        gzipcmd = "gzip " + pipes.quote(TODAYBACKUPPATH) + "/" + db + ".sql"
+        os.system(gzipcmd)
+        p = p + 1
+    dbfile.close()
+    else:
+    db = DB_NAME
+    dumpcmd = "mysqldump -h " + DB_HOST + " -u " + DB_USER + " -p" + DB_USER_PASSWORD + " " + db + " > " + pipes.quote(TODAYBACKUPPATH) + "/" + db + ".sql"
+    os.system(dumpcmd)
+    gzipcmd = "gzip " + pipes.quote(TODAYBACKUPPATH) + "/" + db + ".sql"
+    os.system(gzipcmd)
 
-   while p <= flength:
-       db = dbfile.readline()   # reading database name from file
-       db = db[:-1]         # deletes extra line
-       dumpcmd = "mysqldump -h " + DB_HOST + " -u " + DB_USER + " -p" + DB_USER_PASSWORD + " " + db + " > " + pipes.quote(TODAYBACKUPPATH) + "/" + db + ".sql"
-       os.system(dumpcmd)
-       gzipcmd = "gzip " + pipes.quote(TODAYBACKUPPATH) + "/" + db + ".sql"
-       os.system(gzipcmd)
-       p = p + 1
-   dbfile.close()
-else:
-   db = DB_NAME
-   dumpcmd = "mysqldump -h " + DB_HOST + " -u " + DB_USER + " -p" + DB_USER_PASSWORD + " " + db + " > " + pipes.quote(TODAYBACKUPPATH) + "/" + db + ".sql"
-   os.system(dumpcmd)
-   gzipcmd = "gzip " + pipes.quote(TODAYBACKUPPATH) + "/" + db + ".sql"
-   os.system(gzipcmd)
-
-print ("")
-print ("Backup script completed")
-print ("Your backups have been created in '" + TODAYBACKUPPATH + "' directory")
+    print ("")
+    print ("Backup script completed")
+    print ("Your backups have been created in '" + TODAYBACKUPPATH + "' directory")
